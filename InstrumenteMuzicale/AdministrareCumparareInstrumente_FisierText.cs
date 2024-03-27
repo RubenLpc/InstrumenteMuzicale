c1db52fa6cacddab5ae0f14aa45c4edb0b9241cc
RubenLpc <lupancuruben2@gmail.com>
2024-03-27 22:47:58 +0200

    Clasa cumparare instrument

---
+++ InstrumenteMuzicale/AdministrareCumparareInstrumente_FisierText.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace InstrumenteMuzicale
+{
+    public class AdministrareCumparareInstrumente_FisierText
+    {
+        private string numeFisier;
+
+        public AdministrareCumparareInstrumente_FisierText(string numeFisier)
+        {
+            this.numeFisier = numeFisier;
+            // Dacă fișierul nu există, acesta va fi creat automat când se va încerca să se scrie în el
+        }
+
+        public void AdaugaCumparare(CumparareInstrumentMuzical cumparare)
+        {
+            using (StreamWriter streamWriterFisierText = new StreamWriter(numeFisier, true))
+            {
+                streamWriterFisierText.WriteLine(cumparare.ConversieLaSir_PentruFisier());
+            }
+        }
+
+        public CumparareInstrumentMuzical[] GetCumparari(out int nrCumparari)
+        {
+            CumparareInstrumentMuzical[] cumparari = new CumparareInstrumentMuzical[0];
+
+            using (StreamReader streamReader = new StreamReader(numeFisier))
+            {
+                string linieFisier;
+                nrCumparari = 0;
+
+                while ((linieFisier = streamReader.ReadLine()) != null)
+                {
+                    // Se creează un obiect de tip CumparareInstrumentMuzical pe baza datelor din linia citită
+                    cumparari = cumparari.Append(new CumparareInstrumentMuzical(linieFisier)).ToArray();
+                    nrCumparari++;
+                }
+            }
+
+            return cumparari;
+        }
+
+        public void SalvareCumparareInstrumente()
+        {
+            Console.WriteLine("Datele despre cumpărările de instrumente au fost salvate.");
+        }
+    }
+}
--- InstrumenteMuzicale/Program.cs
+++ InstrumenteMuzicale/Program.cs
@@ -11,11 +11,15 @@ namespace InstrumenteMuzicale
             string folderProiect = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
             string folderInstrumente = Path.Combine(folderProiect, "InstrumenteMuzicale");
             string numeFisierInstrumente = "instrumente.txt";
-            string caleCompletaFisier = Path.Combine(folderInstrumente, numeFisierInstrumente);
+            string numeFisierCumparari = "cumparari_instrumente.txt";
+            string caleCompletaFisierInstrumente = Path.Combine(folderInstrumente, numeFisierInstrumente);
+            string caleCompletaFisierCumparari = Path.Combine(folderInstrumente, numeFisierCumparari);
 
-            AdministrareInstrumente_FisierText adminInstrumente = new AdministrareInstrumente_FisierText(caleCompletaFisier);
+            AdministrareInstrumente_FisierText adminInstrumente = new AdministrareInstrumente_FisierText(caleCompletaFisierInstrumente);
+            AdministrareCumparareInstrumente_FisierText adminCumparari = new AdministrareCumparareInstrumente_FisierText(caleCompletaFisierCumparari);
             InstrumentMuzical instrumentNou = new InstrumentMuzical();
             int nrInstrumente = 0;
+            int nrCumparari = 0;
 
             string optiune;
             do
@@ -24,6 +28,10 @@ namespace InstrumenteMuzicale
                 Console.WriteLine("I. Afisarea informațiilor despre ultimul instrument muzical introdus");
                 Console.WriteLine("A. Afisare instrumente muzicale");
                 Console.WriteLine("S. Salvare instrument muzical");
+                Console.WriteLine("P. Adauga cumparare instrument");
+                Console.WriteLine("F. Afisare cumparari instrumente");
+                Console.WriteLine("G. Salvare cumparari instrumente");
+                Console.WriteLine("N. Cautare instrument dupa nume");
                 Console.WriteLine("X. Inchidere program");
 
                 Console.WriteLine("Alegeti o opțiune");
@@ -33,7 +41,6 @@ namespace InstrumenteMuzicale
                 {
                     case "C":
                         instrumentNou = CitireInstrumentTastatura();
-                        
                         break;
                     case "I":
                         AfisareInstrument(instrumentNou);
@@ -45,6 +52,22 @@ namespace InstrumenteMuzicale
                     case "S":
                         adminInstrumente.AddInstrument(instrumentNou);
                         adminInstrumente.SalvareInstrumente();
+                        break;
+                    case "P":
+                        CumparareInstrumentMuzical cumparareNoua = CitireCumparareTastatura();
+                        adminCumparari.AdaugaCumparare(cumparareNoua);
+                        break;
+                    case "F":
+                        CumparareInstrumentMuzical[] cumparari = adminCumparari.GetCumparari(out nrCumparari);
+                        AfisareCumparari(cumparari);
+                        break;
+                    case "G":
+                        adminCumparari.SalvareCumparareInstrumente();
+                        break;
+                    case "N":
+                        Console.WriteLine("Introduceti numele instrumentului de cautat:");
+                        string numeInstrumentCautat = Console.ReadLine();
+                        CautareInstrumentDupaNume(adminInstrumente, numeInstrumentCautat);
                         break;
                     case "X":
                         return;
@@ -100,5 +123,70 @@ namespace InstrumenteMuzicale
                 }
             }
         }
+
+        public static CumparareInstrumentMuzical CitireCumparareTastatura()
+        {
+            Console.WriteLine("Introduceti numele clientului");
+            string numeClient = Console.ReadLine();
+
+            Console.WriteLine("Introduceti numele instrumentului");
+            string numeInstrument = Console.ReadLine();
+
+            Console.WriteLine("Introduceti tipul instrumentului");
+            string tipInstrument = Console.ReadLine();
+
+            Console.WriteLine("Introduceti pretul instrumentului");
+            decimal pret;
+            while (!decimal.TryParse(Console.ReadLine(), out pret))
+            {
+                Console.WriteLine("Introduceti un pret valid (numar decimal).");
+            }
+
+            Console.WriteLine("Introduceti data cumpararii (Format: dd.mm.yyyy)");
+            DateTime dataCumparari;
+            while (!DateTime.TryParseExact(Console.ReadLine(), "dd.MM.yyyy", null, System.Globalization.DateTimeStyles.None, out dataCumparari))
+            {
+                Console.WriteLine("Introduceti o data valida in formatul dd.mm.yyyy.");
+            }
+
+            CumparareInstrumentMuzical cumparare = new CumparareInstrumentMuzical(numeClient, numeInstrument, tipInstrument, pret, dataCumparari);
+
+            return cumparare;
+        }
+
+        public static void AfisareCumparari(CumparareInstrumentMuzical[] cumparari)
+        {
+            Console.WriteLine("Cumpararile instrumentelor sunt:");
+            foreach (CumparareInstrumentMuzical cumparare in cumparari)
+            {
+                if (cumparare != null)
+                {
+                    string infoCumparare = cumparare.Info();
+                    Console.WriteLine(infoCumparare);
+                }
+            }
+        }
+
+        public static void CautareInstrumentDupaNume(AdministrareInstrumente_FisierText adminInstrumente, string numeInstrumentCautat)
+        {
+            InstrumentMuzical[] instrumente = adminInstrumente.GetInstrumente(out int nrInstrumente);
+            bool instrumentGasit = false;
+
+            foreach (InstrumentMuzical instrument in instrumente)
+            {
+                if (instrument != null && instrument.Nume.Equals(numeInstrumentCautat, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine($"Instrumentul cu numele '{numeInstrumentCautat}' a fost gasit:");
+                    AfisareInstrument(instrument);
+                    instrumentGasit = true;
+                }
+            }
+
+            if (!instrumentGasit)
+            {
+                Console.WriteLine($"Nu s-a gasit niciun instrument cu numele '{numeInstrumentCautat}'.");
+            }
+        }
     }
 }
+
